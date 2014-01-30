97425a5b80f8598e14f9443cbf8136148be7760b
Hobofan <goisser94@gmail.com>
2014-01-30 12:32:39 +0000

    might fix zsh charging bar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -151,7 +151,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     --
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_q, xK_w, xK_e] [0..]
+        | (key, sc) <- zip [xK_e, xK_w, xK_q] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 
